a541f73bfedb3dbd4adce2cfa3df9946e3137862
hvnwntw8 <ryan.m.lee89@gmail.com>
2020-03-24 13:00:33 -0700

    configured mongoDB atlas

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+
+//db connection
+mongoose.connect(
+  process.env.MONGO_URI,
+  {useNewUrlParser: true, useUnifiedTopology: true}
+)
+.then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
+});
+
 
 //bring in routes
 const postRoutes = require('./routes/post')
