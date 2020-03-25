cfd597328ab85ba0a7556c22cf252817c42bed3f
hvnwntw8 <ryan.m.lee89@gmail.com>
2020-03-25 10:55:57 -0700

    creating user schema

--- validator/index.js
+++ validator/index.js
@@ -1,6 +1,6 @@
 exports.createPostValidator = (req, res, next) => {
     // title
-    req.check('title', 'Write a title').notEmpty();
+    req.check('title', 'Please write a title').notEmpty();
     req.check('title', 'Title must be between 4 to 150 characters').isLength({
         min: 4,
         max: 150
