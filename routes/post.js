fd6872caca65e897eec6d1f393fbc7791d2feaa5
hvnwntw8 <ryan.m.lee89@gmail.com>
2020-03-31 11:17:17 -0700

    created deletePost method

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,11 @@
 const express = require('express')
-const { getPosts, createPost, postsByUser } = require('../controllers/post')
+const { getPosts,
+  createPost,
+  postsByUser,
+  postById,
+  isPoster,
+  deletePost
+} = require('../controllers/post')
 const { requireSignin } = require('../controllers/auth')
 const { userById } = require('../controllers/user')
 const { createPostValidator } = require('../validator')
@@ -7,12 +13,19 @@ const { createPostValidator } = require('../validator')
 const router = express.Router()
 
 router.get("/", getPosts);
-router.post("/post/new/:userId", requireSignin, createPost, createPostValidator);
+router.post("/post/new/:userId",
+  requireSignin,
+  createPost,
+  createPostValidator);
 
-router.get("/posts/by/:userId", postsByUser)
+router.get("/posts/by/:userId", requireSignin, postsByUser)
+router.delete("/post/:postId", requireSignin, isPoster, deletePost)
 
 // any route containing :userId, our app will first execute userById()
 router.param("userId", userById);
+
+// any route containing :postId, our app will first execute postById()
+router.param("postId", postById);
 
 
 
